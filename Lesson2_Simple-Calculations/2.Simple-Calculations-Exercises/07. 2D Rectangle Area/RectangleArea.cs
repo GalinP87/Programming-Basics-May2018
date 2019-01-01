df08945ed7d11e2c1a13fd71420fa419305574b7
GalinP87 <43542324+GalinP87@users.noreply.github.com>
2019-01-01 20:55:30 +0200

    Add files via upload

---
+++ Lesson2_Simple-Calculations/2.Simple-Calculations-Exercises/07. 2D Rectangle Area/RectangleArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._2D_Rectangle_Area
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double sizeA = Math.Abs(x1 - x2);
+            double sizeB = Math.Abs(y1 - y2);
+            double area = sizeA * sizeB;
+            double Perimeter = 2 * (sizeA + sizeB);
+            Console.WriteLine(area);
+            Console.WriteLine(Perimeter);
+        }
+    }
+}
